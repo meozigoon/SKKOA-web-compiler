5399c7a4b8d88a644a228f3fbad77211b08b2a6d
DH.L <107869892+meozigoon@users.noreply.github.com>
2025-06-10 09:26:18 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('runButton').addEventListener('click', function() {
+    // 실행 기능 미구현
+});
+document.getElementById('saveButton').addEventListener('click', function() {
+    // 저장 기능 미구현
+});
+document.getElementById('stopButton').addEventListener('click', function() {
+    // 정지 기능 미구현
+});
+document.getElementById('githubButton').addEventListener('click', function() {
+    window.open('https://github.com/', '_blank');
+});
